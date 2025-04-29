f724fd81a94f2b7e8dd3e2b1205bc505dc195e93
Peter Lobo <akumaokami10@gmail.com>
2025-04-29 16:47:29 -0500

    chore: invalidate old sentry dns

--- sentry.io.ts
+++ sentry.io.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/react";
 
 export const initSentry = () =>
   Sentry.init({
-    dsn: "https://5763a46a4251a6fdcd5515157ed50e78@o4508792289624064.ingest.us.sentry.io/4508792290672640",
+    dsn: "<sentry_dns>",
     integrations: [
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration(),
